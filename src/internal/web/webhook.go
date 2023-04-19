8eb4a6b87bc1079bc7cb35a2280c0ecbe7af7ab2
Øystein Qvigstad <oysteinq@stud.ntnu.no>
2023-04-19 23:42:47 +0200

    refactored slightly and added some comments

--- src/internal/web/webhook.go
+++ src/internal/web/webhook.go
@@ -3,6 +3,7 @@ package web
 import (
 	"assignment2/internal/firebase_client"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"log"
 	"math/rand"
@@ -70,16 +71,25 @@ func DelWebhookRegistration(webhookId string) bool {
 	return false
 }
 
+// generateWebhookID is a function that generates a unique 13-character
+// alphanumeric string to be used as a webhook ID.
 func generateWebhookID() string {
+	// Define the character set for the webhook ID
 	const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+	// Set the desired length of the webhook ID
 	length := 13
 	b := make([]byte, length)
+
+	// Continue as long as the newly generated Webhook ID has not been proven to be unique
 	for {
 		var seededRand = rand.New(rand.NewSource(time.Now().UnixNano()))
 		for i := range b {
 			b[i] = charset[seededRand.Intn(len(charset))]
 		}
-		// ensuring WebhookID is unique
+
+		// Check if the generated webhook ID already exists in the registrations map.
+		// If it doesn't exist (i.e., it's unique), break the loop and return the ID.
 		if _, ok := registrations[string(b)]; !ok {
 			break
 		}
@@ -87,41 +97,56 @@ func generateWebhookID() string {
 	return string(b)
 }
 
+// firebaseWorker is a function that runs in the background and periodically
+// sends updates to Firebase. It listens for invocation count and registration
+// updates and sends them to Firebase in bulk every FirebaseUpdateFreq seconds.
 func firebaseWorker() {
+	// Create a new ticker to trigger Firebase updates at a fixed interval
 	ticker := time.NewTicker(FirebaseUpdateFreq * time.Second)
+
+	// Create a new Firebase client
 	client, err := firebase_client.NewFirebaseClient()
 	if err != nil {
 		log.Fatal("Could not start firebase client")
 	}
 
+	// Initialize an empty bundled update to store updates before sending them to Firebase
 	updates := firebase_client.NewBundledUpdate()
 
+	// Start an infinite loop to listen for updates and send them to Firebase
 	for {
 		select {
-		// increment the invocation count and add it to the list of
-		// countries that should be updated
+		// If there's a new invocation count update, add it to the updates
+		// and set the Ready flag to indicate that updates are available
 		case countryCode := <-invocateChannel:
 			updates.InvocationCount[countryCode] = invocationCount[countryCode]
 			updates.Ready = true
 
+		// If there's a new registration update, add it to the updates
+		// and set the Ready flag to indicate that updates are available
 		case action := <-registrationChannel:
 			updates.Registrations[action.Registration.WebhookID] = action
 			updates.Ready = true
 
-			// every 5 seconds, send updates in bulk
+		// When the ticker triggers, check if there are updates to send and
+		// send them to Firebase in bulk if there are. Reset the updates
+		// and Ready flag afterward.
 		case <-ticker.C:
 			if updates.Ready {
 				client.BulkWrite(updates)
 				updates = firebase_client.NewBundledUpdate()
-
 			}
-
 		}
 	}
 }
 
+// registerWebhook is an HTTP handler function that registers a new webhook
+// by decoding an incoming JSON request and validating the data.
 func registerWebhook(w http.ResponseWriter, r *http.Request) {
+	// create a new InvocationRegistration struct to store the decoded data
 	var data firebase_client.InvocationRegistration
+
+	// decode the request body into the InvocationRegistration struct
 	err := json.NewDecoder(r.Body).Decode(&data)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
@@ -129,29 +154,42 @@ func registerWebhook(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	if data.Calls < 1 {
-		http.Error(w, "Number of calls must be 1 or higher", http.StatusBadRequest)
+	// validating the JSON input
+	if err := validateRegistrationData(data); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
 	}
 
-	if !strings.HasPrefix(data.URL, "http://") && !strings.HasPrefix(data.URL, "https://") {
-		http.Error(w, "URL must be prefixed by http:// or https://", http.StatusBadRequest)
-	}
-
-	if _, ok := invocationCount[data.Country]; !ok {
-		http.Error(w, "Country not recognized", http.StatusBadRequest)
-	}
-
+	// adding registration to data structure and notifying firestore that the registration
+	// can be backup up
 	data.WebhookID = generateWebhookID()
-	newEntry := firebase_client.RegistrationAction{
-		Add:          true,
-		Registration: data,
-	}
+	newEntry := firebase_client.RegistrationAction{Add: true, Registration: data}
 	registrations[data.WebhookID] = data
 	registrationChannel <- newEntry
 
-	for key, val := range registrations {
-		fmt.Println(key, val)
+	// Respond 200 OK to the client
+	httpRespondJSON(w, map[string]interface{}{"webhook_id": data.WebhookID})
+}
+
+// validateRegistrationData is a function that takes an InvocationRegistration
+// struct as input and returns an error if the registration data is invalid.
+func validateRegistrationData(registration firebase_client.InvocationRegistration) error {
+	// Check if the number of calls is less than 1. If so, return an error.
+	if registration.Calls < 1 {
+		return errors.New("Number of calls must be 1 or higher")
 	}
 
-	httpRespondJSON(w, map[string]interface{}{"webhook_id": data.WebhookID})
+	// Check if the URL is properly formatted with http:// or https:// prefix.
+	// If not, return an error.
+	if !strings.HasPrefix(registration.URL, "http://") && !strings.HasPrefix(registration.URL, "https://") {
+		return errors.New("URL must be prefixed by http:// or https://")
+	}
+
+	// Check if the country is recognized (i.e., if it exists in the invocationCount map).
+	// If not, return an error.
+	if _, ok := invocationCount[registration.Country]; !ok {
+		return errors.New("Country not recognized")
+	}
+
+	// If all checks pass, return nil, indicating no error occurred.
+	return nil
 }
